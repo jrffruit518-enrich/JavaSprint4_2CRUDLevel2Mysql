7c155704f6754918c70d6160a24bf19e5ab708fe
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-13 21:03:08 +0100

    feat: add  controllers @ tests

---
+++ src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/controllers/FruitController.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.controllers;
+
+public class FruitController {
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/services/ProviderServiceImpTest.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.services;
+
+public class ProviderServiceImpTest {
+}
