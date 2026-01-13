481aad76aedf374569cd2a8eb7c4d0f1a6cf96e6
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-13 23:02:44 +0100

    feat : update ProviderControllerTest

--- src/test/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/controllers/ProviderControllerTest.java
+++ src/test/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/controllers/ProviderControllerTest.java
@@ -1,4 +1,74 @@
 package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.controllers;
 
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.ProviderRequest;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.ProviderResponse;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.eneities.Provider;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.exceptions.ProviderExistsException;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.services.ProviderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProviderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private ProviderService providerService;
+
+    @Test
+    void createProvider_whenValidRequest_shouldReturnProvider() throws Exception {
+        ProviderRequest request = new ProviderRequest("GoodMan", "USA");
+        ProviderResponse response = new ProviderResponse(1L, "GoodMan", "USA");
+        when(providerService.createProvider(any(ProviderRequest.class))).thenReturn(response);
+
+        mockMvc.perform(post("/providers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("GoodMan"))
+                .andExpect(jsonPath("$.country").value("USA"));
+    }
+
+    @Test
+    void createProvider_whenNameExists_shouldReturn409() throws Exception {
+        ProviderRequest request = new ProviderRequest("DuplicateName", "Spain");
+
+        when(providerService.createProvider(any(ProviderRequest.class)))
+                .thenThrow(new ProviderExistsException("Provider already exists."));
+
+        mockMvc.perform(post("/providers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isConflict()) // 验证 409
+                .andExpect(jsonPath("$.message").value("Provider already exists."));
+    }
+
+    @Test
+    void createProvider_whenNameIsEmpty_shouldReturn400() throws Exception {
+        ProviderRequest request = new ProviderRequest("", "USA");
+
+        mockMvc.perform(post("/providers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest()) // 400
+                .andExpect(jsonPath("$.message").exists());
+    }
 }
