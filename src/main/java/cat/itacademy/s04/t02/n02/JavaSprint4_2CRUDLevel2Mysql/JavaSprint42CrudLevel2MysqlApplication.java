98c344a9c2d4873ff67e6c50570952220267dc79
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-13 16:22:46 +0100

    feat: initial commit

---
+++ src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/JavaSprint42CrudLevel2MysqlApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprint42CrudLevel2MysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprint42CrudLevel2MysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/JavaSprint42CrudLevel2MysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprint42CrudLevel2MysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
