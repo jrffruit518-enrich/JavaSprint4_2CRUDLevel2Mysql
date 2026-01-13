d3698272182f6fe642c2196a40e56c95298f2365
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-13 20:36:53 +0100

    feat: add interface of service

---
+++ src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/services/FruitService.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.services;
+
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.FruitRequest;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.FruitResponse;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.eneities.Fruit;
+
+import java.util.List;
+
+public interface FruitService {
+    FruitResponse createFruit(FruitRequest request);
+    List<FruitResponse> findFruitByProviderName(String name);
+}
---
+++ src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/services/ProviderService.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.services;
+
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.ProviderRequest;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.ProviderResponse;
+
+
+public interface ProviderService {
+    ProviderResponse createProvider(ProviderRequest request);
+    ProviderResponse findByName(String name);
+    ProviderResponse updateProviderByName(String name,ProviderRequest request);
+    void deleteProviderByName(String name);
+}
