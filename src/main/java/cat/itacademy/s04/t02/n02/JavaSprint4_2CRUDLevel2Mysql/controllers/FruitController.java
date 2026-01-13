0946681c68777b54cbc4be983be0457bbbb578aa
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-13 23:15:38 +0100

    feat : update FruitController

--- src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/t02/n02/JavaSprint4_2CRUDLevel2Mysql/controllers/FruitController.java
@@ -1,4 +1,22 @@
 package cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.controllers;
 
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.FruitRequest;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.DTO.FruitResponse;
+import cat.itacademy.s04.t02.n02.JavaSprint4_2CRUDLevel2Mysql.services.FruitService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fruits")
 public class FruitController {
+    private final FruitService fruitService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public FruitResponse createFruit(@RequestBody @Valid FruitRequest request) {
+        return fruitService.createFruit(request);
+    }
 }
